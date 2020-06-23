5559e5aba21c5c84303984a182c4e2fecb05e375
Tom Bell <tjbell@gmail.com>
2020-06-23 15:16:59 -0600

    fix: adjust names

--- web.js
+++ web.js
@@ -2,15 +2,11 @@ const { message } = require('./utils.js');
 
 const createBucket = (name) => {};
 
-const listBuckets = (name) => {};
-
 const upload = (name) => {};
 
-const addBucketPermissions = (name) => {};
+const enableWebsiteConfig = (name) => {};
 
-const addBucketPolices = (name) => {};
-
-const deleteBucket = (name) => {};
+const disableWebsiteConfig = (name) => {};
 
 
 /****
@@ -18,11 +14,9 @@ const deleteBucket = (name) => {};
 ****/
 const cli = require('./cli.js');
 switch (cli.command) {
-  case 'create': createBucket(cli.resourceName); break;
-  case   'list': listBuckets(cli.resourceName); break;
-  case 'upload': upload(cli.resourceName); break;
-  case 'access': addBucketPermissions(cli.resourceName); break;
-  case 'policy': addBucketPolices(cli.resourceName); break;
-  case 'delete': deleteBucket(cli.resourceName); break;
-  default      : console.error('Not a valid command!'); break;
+  case  'create': createBucket(cli.resourceName); break;
+  case  'upload': upload(cli.resourceName); break;
+  case  'enable': enableWebsiteConfig(cli.resourceName); break;
+  case 'disable': disableWebsiteConfig(cli.resourceName); break;
+  default       : console.error('Not a valid command!'); break;
 }
