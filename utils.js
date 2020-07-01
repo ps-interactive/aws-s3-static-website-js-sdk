6deb5952c1e9e2352233e603e947b609a3a6b328
Tom Bell <tjbell@gmail.com>
2020-07-01 12:24:10 -0600

    fix: upload directory structure

--- utils.js
+++ utils.js
@@ -9,13 +9,7 @@ const s3 = new AWS.S3();
 
 const message = (err, data) => {
   if (err) { console.log(`Error: ${err.message}`); }
-  else if (data) {
-    const json = JSON.stringify(data);
-    if(Object.keys(data).length > 1) {
-      fs.writeFileSync(`json/${Object.keys(data)[1]}.json`, json, 'utf-8');
-    }
-    console.log(`Success: ${json}`);
-  }
+  else if (data) { console.log(`Success: ${JSON.stringify(data, null, 2)}`); }
 };
 
 const paths = (dir, container) => {
