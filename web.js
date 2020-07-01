972bd5ecd56fd4dc31d0af932a46b39b2471b4b8
Tom Bell <tjbell@gmail.com>
2020-07-01 11:12:20 -0600

    solution: remove

--- web.js
+++ web.js
@@ -3,40 +3,19 @@ const path = require('path');
 
 const { message, paths, upload } = require('./utils.js');
 
-const AWS = require('aws-sdk');
-AWS.config.region = 'us-west-2';
-AWS.config.apiVersions = { 's3': '2006-03-01' };
+/******************
+ AWS Configuration 
+******************/
 
-const s3 = new AWS.S3();
 
-const createUpload = (name, dir) => {
-  s3.createBucket({ "Bucket": name }, (err, data) => {
-    if (err) { console.log("Error", err); }
-    else {
-      const files = paths(dir);
-      files.forEach(file => upload(name, file));
-    }
-  });
-};
 
-const enableWebsiteConfig = (name, index, error) => {
-  const params = {
-    "Bucket": name,
-    "WebsiteConfiguration": {
-      "IndexDocument": { "Suffix": index },
-      "ErrorDocument": { "Key": error },
-    }
-  };
-  s3.putBucketWebsite(params, message);
-};
+const createUpload = (name, dir) => {};
 
-const getWebsiteConfig = (name) => {
-  s3.getBucketWebsite({ "Bucket": name }, message);
-};
+const enableWebsiteConfig = (name, index, error) => {};
 
-const disableWebsiteConfig = (name) => {
-  s3.deleteBucketWebsite({ "Bucket": name }, message);
-};
+const getWebsiteConfig = (name) => {};
+
+const disableWebsiteConfig = (name) => {};
 
 /****
  CLI 
